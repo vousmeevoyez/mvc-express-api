a30b9fef9e4b91d99880cd7dc743f807fcb77858
unknown <nizarudiny@gmail.com>
2024-07-10 22:27:47 +0700

    menambahkan style inline di html dan mengubah file scema

--- schemas/userSchema.mjs
+++ schemas/userSchema.mjs
@@ -1,10 +1,10 @@
 import Joi from "joi";
 
 const userSchema = Joi.object({
-  firstName: Joi.string().min(3).required(),
-  lastName: Joi.string().min(3).optional(),
+  firstName: Joi.string().pattern(/^[A-Z a-z]+$/).min(3).required(),
+  lastName: Joi.string().pattern(/^[A-Z a-z]+$/).min(3).optional(),
   phoneNumber: Joi.string()
-    .pattern(/^(?:\+62|62|0)8[1-9][0-9]{6,9}$/)
+    .pattern(/^(?:\+62|62|0)8[1-9][0-9]{9,12}$/)
     .required(),
   email: Joi.string().email().required(),
 });
