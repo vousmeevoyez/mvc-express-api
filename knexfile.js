19ff4b92d3223bc86b93ebc6aefdeeb08b7813f6
Kelvin <kelvin@Kelvins-MacBook-Air.local>
2024-08-26 11:47:33 +0700

    chore: update db + knex settings

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,12 @@
 require("dotenv").config();
 
 module.exports = {
-  development: {
-    client: "postgresql",
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_DATABASE,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "./database/migrations/",
-    },
+  client: "postgresql",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+    directory: "./database/migrations/",
   },
 };
